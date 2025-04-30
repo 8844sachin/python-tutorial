1f9601f8f0a1c116ea46978d2372cba29ccaf7f7
8844/sachin <sjsachinjaiswal8844@gmail.com>
2025-04-30 23:05:58 +0530

    datatype in py

---
+++ datatype.py
@@ -0,0 +1,37 @@
+#int datatype
+a=10
+#string datatype
+b="hello"
+#float datatype
+c=40.23
+#complex datatype
+d=4+2j
+#list datatype
+e=["a","b","c"]
+#tuple datatype
+f=("a","b","c")
+#set datatype
+g={"a","b","c"}
+#dictionary datatype
+h={"a":10,"b":20,"c":30}
+#Boolean datatype
+i=False
+
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
+print(type(i))
+
+
+x=str(a)
+print(x)
+y=int(c)
+print(y)
+
+print(type(y))
