c343eac2d50481c5417993a482c9df3d384f0d92
8844/sachin <sjsachinjaiswal8844@gmail.com>
2025-04-30 21:32:40 +0530

    comment added

--- hello.py
+++ hello.py
@@ -1,2 +1,10 @@
 print("Python Developer")
 print("Hello World")
+
+print("Single line comment start with #")
+#This is the single line comment
+
+print("Multiline comment start with three quatation mark")
+'''This is multiline comment
+ it is used for multiline comment '''
+
