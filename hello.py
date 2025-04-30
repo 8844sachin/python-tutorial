35798c8a02022ac73c604f1eb5ffd80085768798
8844/sachin <sjsachinjaiswal8844@gmail.com>
2025-04-30 21:38:12 +0530

    data type and id identify

--- hello.py
+++ hello.py
@@ -8,3 +8,12 @@ print("Multiline comment start with three quatation mark")
 '''This is multiline comment
  it is used for multiline comment '''
 
+x=10
+y="20"
+z=40.2
+print(type(x))
+print(type(y))
+print(type(z))
+print(id(x))
+print(id(y))
+print(id(z))
