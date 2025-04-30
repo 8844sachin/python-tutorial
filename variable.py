dad8b98fc395b4e512e6c86b946d8ae63da97e3d
8844/sachin <sjsachinjaiswal8844@gmail.com>
2025-04-30 22:00:37 +0530

    legal variable and multi val assign to multiple variable

---
+++ variable.py
@@ -0,0 +1,25 @@
+#This is legal variable
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+'''Illegal variable name
+2myvar = "John"
+my-var = "John"
+my var = "John"
+'''
+
+#Multiple value assign with multiple variable
+x,y,z="morning","afternoon","evening"
+print(x)
+print(y)
+print(z)
+
+#Single value assign to multiple variable
+a=b=c=10
+print(c)
+print(a)
+print(a+c)
